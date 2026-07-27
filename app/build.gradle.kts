05f75a09b76fcbb334c71f5d578212839e9ae6aa
melastore <melastore@users.noreply.github.com>
2026-07-27 21:16:49 +0300

    Release 0.4.3
    Changelogs for everything since 0.2.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
 		applicationId = "io.github.melastore.shelf"
 		minSdk = 30
 		targetSdk = 36
-		versionCode = 2
-		versionName = "0.2.0"
+		versionName = "0.4.3"
+		versionCode = 7
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 
