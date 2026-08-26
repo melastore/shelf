bd2298509f11be123769f4373d32dc68eb7ba36d
melastore <melastore@users.noreply.github.com>
2026-08-26 07:28:05 +0300

    Release 0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
 		applicationId = "io.github.melastore.shelf"
 		minSdk = 30
 		targetSdk = 37
-		versionName = "0.5.0"
-		versionCode = 9
+		versionName = "0.5.1"
+		versionCode = 10
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 
